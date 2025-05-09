7c8be4df08c2c86dfc1afa21afe53ecfe17f4ca9
mbnczy <108935376+mbnczy@users.noreply.github.com>
2025-05-09 16:01:53 +0200

    add slack callback

--- callbacks.py
+++ callbacks.py
@@ -7,6 +7,11 @@ from tqdm import tqdm
 from sklearn.metrics import confusion_matrix
 import pandas as pd
 
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+import time
+
 class LLMSampleCallback(WandbCallback):
     def __init__(self, trainer, test_dataset, batch_size=1, num_samples=10, max_new_tokens=256):
         "A CallBack to log samples as a wandb.Table during training"
@@ -108,3 +113,91 @@ class LLMSampleCallback(WandbCallback):
         self._wandb.log({"sample_predictions": self.samples_table(examples)})
 
         self.log_confusion_matrix(examples['y_true'].tolist(), examples['y_preds'].tolist())
+
+
+def custom_eta(trainer):
+    if len(trainer.state.log_history) > 0:
+        last_log = trainer.state.log_history[-1]
+        elapsed_time = sum(log.get('train_runtime', 0) for log in trainer.state.log_history)  # Total elapsed time
+        percent_complete = (trainer.state.global_step / trainer.state.max_steps) * 100
+        
+        if percent_complete > 0:
+            total_estimated_time = elapsed_time * (100 / percent_complete)
+            eta_seconds = total_estimated_time - elapsed_time
+            eta = time.strftime("%H:%M:%S", time.gmtime(eta_seconds))
+        else:
+            eta = "N/A"
+    else:
+        eta = "N/A"
+        
+    return eta
+
+class SlackLogger:
+    def __init__(self, channel_id, slack_bot_token):
+        self.ch_id = channel_id
+        self.b_token = slack_bot_token
+        self.client = WebClient(token=self.b_token)
+        self.main_thread = None
+
+    def init_training(self, params):
+        message = f"🧬 Training started 🧬\n\n"
+        message += "\n".join(f"- {key}: {value}" for key, value in params.items())
+        try:
+            response = self.client.chat_postMessage(
+                channel=self.ch_id,
+                text=message
+            )
+            self.main_thread = response['ts']
+            print(f"Slack message sent: {response['message']['text']}")
+        except SlackApiError as e:
+            print(f"Error sending slack message: {e.response['error']}")
+
+    def log_eval(self, trainer, params):
+        eta = None
+        message = f"🧬 Epoch: {round(trainer.state.epoch,1)}/{trainer.args.num_train_epochs}\t State: {round((trainer.state.global_step / trainer.state.max_steps) * 100,1)}% \tETA: {custom_eta(trainer)}\n\n"
+        message += "\n".join(f"- {key}: {value}" for key, value in params.items())
+        try:
+            reply_response = self.client.chat_postMessage(
+                channel=self.ch_id,
+                text=message,
+                thread_ts=self.main_thread
+            )
+            print(f"Slack thread reply sent: {reply_response['message']['text']}")
+        
+        except SlackApiError as e:
+            print(f"Error sending slack reply: {e.response['error']}")
+
+    def end_training(self):
+        message = "Training finished!"
+        try:
+            reply_response = self.client.chat_postMessage(
+                channel=self.ch_id,
+                text=message,
+                thread_ts=self.main_thread
+            )
+            print(f"Slack thread reply sent: {reply_response['message']['text']}")
+
+            response = self.client.reactions_add(
+                channel=self.ch_id,
+                timestamp=self.main_thread,
+                name="white_check_mark"
+            )
+            print(f"Reaction added")
+        
+        except SlackApiError as e:
+            print(f"Error sending slack reply: {e.response['error']}")
+
+
+
+    def log_cr(self, cr_path):
+        try:
+            reply_response = self.client.files_upload_v2(
+                channel=self.ch_id,
+                file=cr_path,
+                title="Classification Report",
+                thread_ts=self.main_thread
+            )
+            print(f"Slack thread reply sent: {reply_response['message']['text']}")
+        
+        except SlackApiError as e:
+            print(f"Error sending slack reply: {e.response['error']}")
